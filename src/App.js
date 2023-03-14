46504ac5d8fb7eeb01db398a4720d7cad8afc91f
Kadir Alibasic <kadiralibasic123@gmail.com>
2023-03-14 12:45:38 +0100

    Adding reset functionality and own to-dos on load

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import React from "react";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import TodoItem from "./TodoItem";
+import { todos } from "./todos";
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [], // prayan array koji se treba puniti fetchovima
+      todos: todos, // prayan array koji se treba puniti fetchovima
       done: false, // done se koristi kako bi se oznacio todo kao uradjen ili neuradjen
       newTodo: '', // ovaj state key se koristi pri kreiranju novih to-do-ova preko input metoda
     }
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users') //  fetchamo sa int
-      .then(rsp => rsp.json())
-      .then(data => {
-        this.setState({ todos: data }); // punimo todos array sa ovim fetchovima
-      })
-  }
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users') //  fetchamo sa int
+  //     .then(rsp => rsp.json())
+  //     .then(data => {
+  //       this.setState({ todos: data }); // punimo todos array sa ovim fetchovima
+  //     })
+  // }
 
   handleInputChange = (e) => {
     this.setState({ newTodo: e.target.value });
@@ -40,12 +42,15 @@ class App extends React.Component {
   }
 
 
+  handleResetButton = () => {
+    this.setState({ newTodo: '' });
+  }
 
 
   render() {
     return (
-      <div className=" align-middle justify-center">
-        <TodoInput inputChange={this.handleInputChange} submitChange={this.handleSubmitChange} addToDo={this.addTodo} />
+      <div className=" bg-blue-500 p-5 absolute">
+        <TodoInput inputChange={this.handleInputChange} submitChange={this.handleSubmitChange} value={this.state.newTodo} handleResetButton={this.handleResetButton} />
         <TodoItem />
         <TodoList todos={this.state.todos} />
       </div>
--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-const TodoInput = ({ inputChange, submitChange }) => {
+const TodoInput = ({ inputChange, submitChange, value, handleResetButton }) => {
     return (
         <div>
             <form onSubmit={submitChange}>
-                <input type="text" placeholder="enter to-do" onChange={inputChange} />
+                <input type="text" placeholder="enter to-do" onChange={inputChange} value={value} />
                 <button onClick={submitChange} type="submit">Enter</button>
-                <button type="reset" className="ml-3">Reset</button>
+                <button type="reset" className="ml-3" onClick={handleResetButton}>Reset</button>
             </form>
         </div>
     )
