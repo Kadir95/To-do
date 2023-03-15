2811938d6bd5c6439915e4ef6be5825c1d494e59
Kadir Alibasic <kadiralibasic123@gmail.com>
2023-03-15 20:51:29 +0100

    Added todos.js file

---
+++ src/todos.js
@@ -0,0 +1,19 @@
+export const todos = [
+    {
+        name: "get up early",
+        id: 1,
+    },
+    {
+        name: "make bed",
+        id: 2
+    },
+    {
+        name: "drink water",
+        id: 3
+    },
+    {
+        name: "stretch",
+        id: 4
+    },
+]
+
